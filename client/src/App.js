ecb87f4a0f9f25d72fc475cd37283f49dfcf451a
WXZVRD <134217021+WXZVRD@users.noreply.github.com>
2023-10-27 18:09:22 +0600

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -24,8 +24,6 @@ function App() {
         const fetchUsersList = async () => {
             try {
                 const response = await axios.get('http://localhost:3000');
-                console.log(response.data)
-                console.log('work!')
                 setUsers(response.data);
             } catch (error) {
                 console.error('Error fetching users:', error);
